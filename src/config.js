78854179a8e45041a541f1a9681e83518a9843bc
Naftali Horviz <n0548498093@gmail.com>
2025-03-26 12:15:17 +0200

    5

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    backendUrl: "https://test-back-ty38.onrender.com"  // כאן תכתוב את ה-URL שלך
+    // backendUrl: "http://localhost:5000"  // כאן תכתוב את ה-URL שלך
+  };
+  
+  export default config;
+  
