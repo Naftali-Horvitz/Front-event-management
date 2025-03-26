cf575d35bdf0b7c23f3b2f790c77106663ff5e2f
Naftali Horviz <n0548498093@gmail.com>
2025-03-26 12:25:19 +0200

    8

--- src/components/auth/SignupHost.jsx
+++ src/components/auth/SignupHost.jsx
@@ -5,7 +5,7 @@ import { Loader2 } from "lucide-react";
 import config from "../../../src/config";
 import { validateField, validateForm } from "../../utils/validation";
 import { setNewUserData, isTokenPresent } from "../../utils/authUtils";
-import { useEventContext } from "../../context/eventDataContext";
+import { useEventContext } from "../../context/EventDataContext";
 import { useAuth } from "../../context/AuthContext";
 
 const SignupForm = () => {
